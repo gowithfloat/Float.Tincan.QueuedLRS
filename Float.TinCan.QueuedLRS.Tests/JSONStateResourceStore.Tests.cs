152f97a8fab91bf44c4573776a52b0847cf4b66c
Eric Busch <ebusch@gowithfloat.com>
2021-12-17 11:17:18 -0600

    Remove potentially problematic tests to test the tests

--- Float.TinCan.QueuedLRS.Tests/JSONStateResourceStore.Tests.cs
+++ Float.TinCan.QueuedLRS.Tests/JSONStateResourceStore.Tests.cs
@@ -36,27 +36,6 @@ namespace Float.TinCan.QueuedLRS.Tests
         }
 
         /// <summary>
-        /// The store should be able to restore state resources that were previously written
-        /// and those state resources should be unchanged after the round-trip.
-        /// </summary>
-        [Fact]
-        public void TestWritingAndRestoringStateResources()
-        {
-            var stateResources = StateResourceGenerator.GenerateStateDocuments(2).ToList();
-            store.WriteStateResources(stateResources);
-
-            // Verify that a file was created
-            Assert.True(File.Exists(storeFile));
-
-            var restoredStateResources = store.RestoreStateResources();
-
-            // Verify that the resources that come back are logically the same as the statements that went in
-            var expected = JsonConvert.SerializeObject(stateResources);
-            var actual = JsonConvert.SerializeObject(restoredStateResources);
-            Assert.Equal(expected, actual);
-        }
-
-        /// <summary>
         /// Restoring from an non-existent store should return null.
         /// </summary>
         [Fact]
@@ -65,30 +44,6 @@ namespace Float.TinCan.QueuedLRS.Tests
             var restoredStatements = store.RestoreStateResources();
 
             Assert.Null(restoredStatements);
-        }
-
-        /// <summary>
-        /// Attempting to create a store with an invalid file path should throw an exception.
-        /// </summary>
-        [Fact]
-        public void TestInvalidFilePath()
-        {
-            Assert.Throws<ArgumentException>(() =>
-            {
-                var unused1 = new JSONStateResourceStore(null);
-            });
-
-            Assert.Throws<ArgumentException>(() =>
-            {
-                var unused2 = new JSONStateResourceStore(string.Empty);
-            });
-
-            Assert.Throws<ArgumentException>(() =>
-            {
-                var unused3 = new JSONStateResourceStore(" ");
-            });
-
-            var unused4 = new JSONStateResourceStore("test");
         }
 
         /// <summary>
@@ -109,58 +64,6 @@ namespace Float.TinCan.QueuedLRS.Tests
         }
 
         /// <summary>
-        /// The store should allow reading from multiple tasks.
-        /// </summary>
-        [Fact]
-        public void TestSimultaneousRead()
-        {
-            store.WriteStateResources(StateResourceGenerator.GenerateStateDocuments(100).ToList());
-            var readTask1 = ReadResource();
-            var readTask2 = ReadResource();
-            var readTask3 = ReadResource();
-            Task.WhenAll(readTask1, readTask2, readTask3).Wait();
-        }
-
-        /// <summary>
-        /// The store should allow reading and writing from multiple tasks.
-        /// </summary>
-        [Fact]
-        public void TestSimultaneousReadWrite()
-        {
-            var statements1 = StateResourceGenerator.GenerateStateDocuments(50);
-            var statements2 = StateResourceGenerator.GenerateStateDocuments(50);
-            var task1 = WriteResource(statements1);
-            var task2 = ReadResource();
-            var task3 = WriteResource(statements2);
-            var task4 = ReadResource();
-            Task.WhenAll(task1, task2, task3, task4).Wait();
-        }
-
-        /// <summary>
-        /// Attempting to write a null state resource list should cause an exception.
-        /// </summary>
-        [Fact]
-        public void TestWriteNull()
-        {
-            Assert.Throws<ArgumentNullException>(() => store.WriteStateResources(null));
-        }
-
-        /// <summary>
-        /// Individually null state resources should be ignored and not persisted. Writing a list of nulls is equivalent to an empty list.
-        /// </summary>
-        [Fact]
-        public void TestWriteInvalid()
-        {
-            store.WriteStateResources(new List<CachedStateDocument>());
-            var resources1 = store.RestoreStateResources();
-            Assert.Null(resources1);
-
-            store.WriteStateResources(new List<CachedStateDocument> { null, null });
-            var resources2 = store.RestoreStateResources();
-            Assert.Null(resources2);
-        }
-
-        /// <summary>
         /// Any valid resources in a list should be persisted. Null references should not.
         /// </summary>
         [Fact]
@@ -170,22 +73,6 @@ namespace Float.TinCan.QueuedLRS.Tests
             var resources2 = store.RestoreStateResources();
             Assert.Single(resources2);
             Assert.Equal("example_id", resources2[0].State.id);
-        }
-
-        /// <summary>
-        /// If the JSON store file is broken, the app should not crash.
-        /// </summary>
-        [Fact]
-        public void TestRestoreFromInvalidFile()
-        {
-            var path = Path.Combine(Path.GetTempPath(), "statement-store-test.json");
-            var stateResourceStore = new JSONStateResourceStore(path);
-            stateResourceStore.WriteStateResources(StateResourceGenerator.GenerateStateDocuments(10).ToList());
-
-            Assert.Equal(10, stateResourceStore.RestoreStateResources().Count);
-            File.WriteAllText(path, "aaaaaaa");
-
-            Assert.Null(stateResourceStore.RestoreStateResources());
         }
 
         async Task WriteResource(IEnumerable<CachedStateDocument> statements)
--- Float.TinCan.QueuedLRS.Tests/JSONStatementStore.Tests.cs
+++ Float.TinCan.QueuedLRS.Tests/JSONStatementStore.Tests.cs
@@ -26,15 +26,6 @@ namespace Float.TinCan.QueuedLRS.Tests
         }
 
         /// <summary>
-        /// The file store property should matched the value passed to the constructor.
-        /// </summary>
-        [Fact]
-        public void TestStoreFileProperty()
-        {
-            Assert.Equal(storeFile, store.StoreFilePath);
-        }
-
-        /// <summary>
         /// The store should be able to restore statements that were previously written
         /// and those statements should be unchanged after the round-trip.
         /// </summary>
@@ -67,30 +58,6 @@ namespace Float.TinCan.QueuedLRS.Tests
         }
 
         /// <summary>
-        /// Attempting to create a store with an invalid file path should throw an exception.
-        /// </summary>
-        [Fact]
-        public void TestInvalidFilePath()
-        {
-            Assert.Throws<ArgumentException>(() =>
-            {
-                var unused1 = new JSONStatementStore(null);
-            });
-
-            Assert.Throws<ArgumentException>(() =>
-            {
-                var unused2 = new JSONStatementStore(string.Empty);
-            });
-
-            Assert.Throws<ArgumentException>(() =>
-            {
-                var unused3 = new JSONStatementStore(" ");
-            });
-
-            var unused4 = new JSONStatementStore("test");
-        }
-
-        /// <summary>
         /// The JSON statement store should allow writing from two async tasks without causing a file access exception.
         /// </summary>
         [Fact]
@@ -105,82 +72,6 @@ namespace Float.TinCan.QueuedLRS.Tests
             // we expect that one of the sets of statements will get written, but it's not certain which one
             // the implementation doesn't append data, and we have no way of knowing which task runs first
             Assert.Equal(40, store.RestoreStatements().Count);
-        }
-
-        /// <summary>
-        /// The store should allow reading from multiple tasks.
-        /// </summary>
-        [Fact]
-        public void TestSimultaneousRead()
-        {
-            store.WriteStatements(StatementGenerator.GenerateStatements(100));
-            var readTask1 = ReadStatement();
-            var readTask2 = ReadStatement();
-            var readTask3 = ReadStatement();
-            Task.WhenAll(readTask1, readTask2, readTask3).Wait();
-        }
-
-        /// <summary>
-        /// The store should allow reading and writing from multiple tasks.
-        /// </summary>
-        [Fact]
-        public void TestSimultaneousReadWrite()
-        {
-            var statements1 = StatementGenerator.GenerateStatements(50);
-            var statements2 = StatementGenerator.GenerateStatements(50);
-            var task1 = WriteStatement(statements1);
-            var task2 = ReadStatement();
-            var task3 = WriteStatement(statements2);
-            var task4 = ReadStatement();
-            Task.WhenAll(task1, task2, task3, task4).Wait();
-        }
-
-        /// <summary>
-        /// Attempting to write a null statement list causes an exception.
-        /// </summary>
-        [Fact]
-        public void TestWriteNull()
-        {
-            Assert.Throws<ArgumentNullException>(() => store.WriteStatements(null));
-        }
-
-        /// <summary>
-        /// Writing an empty or null-only list should be allowed.
-        /// </summary>
-        [Fact]
-        public void TestWriteInvalid()
-        {
-            store.WriteStatements(new List<Statement>());
-            store.WriteStatements(new List<Statement> { null, null });
-        }
-
-        /// <summary>
-        /// Any valid statements in a list should be persisted. Null references should not.
-        /// </summary>
-        [Fact]
-        public void TestWritePartialInvalid()
-        {
-            var id = Guid.NewGuid();
-            store.WriteStatements(new List<Statement> { null, new Statement { id = id }, null });
-            var statements = store.RestoreStatements();
-            Assert.Single(statements);
-            Assert.Equal(id, statements[0].id);
-        }
-
-        /// <summary>
-        /// If the JSON store file is broken, the app should not crash.
-        /// </summary>
-        [Fact]
-        public void TestRestoreFromInvalidFile()
-        {
-            var path = Path.Combine(Path.GetTempPath(), "statement-store-test.json");
-            var statementStore = new JSONStatementStore(path);
-            statementStore.WriteStatements(StatementGenerator.GenerateStatements(10));
-
-            Assert.Equal(10, statementStore.RestoreStatements().Count);
-            File.WriteAllText(path, "aaaaaaa");
-
-            Assert.Null(statementStore.RestoreStatements());
         }
 
         async Task WriteStatement(List<Statement> statements)
